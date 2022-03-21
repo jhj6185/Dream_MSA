8b0662c18c632279025dbf315b4d034756648a9e
DESKTOP-548TSVP\skyjb <skyjbkim88@gmail.com>
2022-03-21 17:53:15 +0900

    [김종백] 3월 21일 1.product 시큐리티 제거(로그인 안해도 상품 목록 확인 가능하기 때문에)

--- product-service/src/main/java/com/dream/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/dream/productservice/ProductServiceApplication.java
@@ -3,8 +3,6 @@ package com.dream.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
